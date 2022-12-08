f4ca94c2cf3684230d8ae9cb79d6dbe2b2096621
Devendra Negi <devendera.negi@gmail.com>
2022-12-08 17:13:23 +0530

    all features working with map bug

--- src/components/MapWrapper.js
+++ src/components/MapWrapper.js
@@ -1,16 +1,24 @@
 import { useEffect, useContext, useState } from 'react'
 import { Wrapper } from '@googlemaps/react-wrapper'
-import { useLoadScript } from "@react-google-maps/api"
+// import { useLoadScript } from "@react-google-maps/api"
 
 import Map from './Map'
 import { getStarWidth, AppContext } from "../utills"
-import { searchActions, filterActions, googleMapsApiConfig } from "../constant"
+import { googleMapsApiConfig } from "../constant"
+
+// function MapWrapper() {
+//     const { isLoaded } = useLoadScript(googleMapsApiConfig)
+//     return (isLoaded ? <Map /> : <div>Loading...</div>)
+// }
 
 function MapWrapper() {
     return (
-        <Wrapper {...googleMapsApiConfig}>
-            <Map />
-        </Wrapper> 
+        <div class="map">
+            <Wrapper {...googleMapsApiConfig}>
+                <Map />
+            </Wrapper> 
+        </div>
+        
     )
 }
 
